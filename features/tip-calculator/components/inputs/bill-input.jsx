8578d641bd6cd07b4ba015cf8e8f4ac0f2792438
Shehzad Ashraf <stewardmax5331@gmail.com>
2026-02-25 11:12:46 +0500

    Add BillInput and PeopleInput components for user input

---
+++ features/tip-calculator/components/inputs/bill-input.jsx
@@ -0,0 +1,5 @@
+import ReusableFormInput from "@/components/common/reusable-form-input";
+
+export default function BillInput() {
+  return <ReusableFormInput label="Bill" icon="$" placeholder="0" />;
+}
---
+++ features/tip-calculator/components/inputs/people-input.jsx
@@ -0,0 +1,7 @@
+import ReusableFormInput from "@/components/common/reusable-form-input";
+
+export default function PeopleInput() {
+  return (
+    <ReusableFormInput label="Number of People" icon="#" placeholder="0" />
+  );
+}
