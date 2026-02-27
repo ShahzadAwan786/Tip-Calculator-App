d94b11b527cd6cb08a996dc449c427cd56f1180f
Shehzad Ashraf <stewardmax5331@gmail.com>
2026-02-27 12:19:36 +0500

    Refactor TipResultPanel and TipSelectorButtons to replace reusable components with updated ResultsRow and FormInput; integrate Controller for improved form handling

--- components/core/tip-result-panel.jsx
+++ components/core/tip-result-panel.jsx
@@ -1,18 +1,19 @@
 "use client";
-import ReusableResultsRow from "@/components/common/reusable-results-row";
 import { Button } from "@/components/ui/button";
 import React from "react";
 import { useTipCalculator } from "./tip-calculator-context";
+import ResultsRow from "../common/results-row";
 
-export default function TipResultPanel() {
-  const { bill, people, reset, tipPerPerson, totalPerPerson } =
-    useTipCalculator();
+export default function TipResultPanel({ methods }) {
+  const { bill, people, tipPerPerson, totalPerPerson } = useTipCalculator();
+  const { reset } = methods;
   const isDisabled = !bill || !people;
+
   return (
     <div className="bg-primary sm:m-4 rounded-2xl p-6 sm:p-8 flex flex-col justify-between">
       <div className="space-y-6">
-        <ReusableResultsRow label="Tip Amount" value={tipPerPerson} />
-        <ReusableResultsRow label="Total" value={totalPerPerson} />
+        <ResultsRow label="Tip Amount" value={tipPerPerson} />
+        <ResultsRow label="Total" value={totalPerPerson} />
       </div>
       <Button
         className="w-full h-12 bg-secondary text-primary text-lg sm:text-xl mt-10 font-bold hover:bg-foreground disabled:bg-[#0d686d]"
